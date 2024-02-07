19cb57016f0d85966cd722309c73a92914f2db76
Almeida <joao.almeidapinto@etu.estia.fr>
2024-02-07 09:40:26 +0100

    dossier views

--- exemple_cours/scripts/sql/user.sql
+++ exemple_cours/scripts/sql/user.sql
@@ -6,28 +6,28 @@
 -- Generation Time: Jan 03, 2024 at 03:33 PM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
-
+ 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
+ 
+ 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
+ 
 --
 -- Database: `tai`
 --
-
+ 
 -- --------------------------------------------------------
-
+ 
 --
 -- Table structure for table `user`
 --
-
+ 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -37,19 +37,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` text NOT NULL,
   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Example of user table for login info';
-
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4  COMMENT='Example of user table for login info';
+ 
 --
 -- Dumping data for table `user`
 --
-
+ 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `login`, `password`, `creation_date`) VALUES
 (1, 'Homer', 'Simpson', 'donut', '202cb962ac59075b964b07152d234b70', '2024-01-03 15:41:56'),
 (2, 'Marge', 'Simpson', 'marge', '519b2f2d0d0c048c6a5d085f79d6012c', '2024-01-03 15:41:56'),
 (3, 'Bart', 'Simpson', 'el barto', '172924aadec293666b805437b84c18d7', '2024-01-03 15:41:56'),
 (4, 'Lisa', 'Simpson', 'lisa_simpson', 'f61f2e52cef0031f01f332033298f9e9', '2024-01-03 15:41:56');
 COMMIT;
-
+ 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
