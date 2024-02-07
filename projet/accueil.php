25d018526f3d8d367a07f802682b3ecfc40f4273
thatgirlAm2 <amaelle.diop@etu.estia.fr>
2024-02-07 08:36:33 +0100

    test

--- projet/accueil.php
+++ projet/accueil.php
@@ -6,6 +6,26 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Bienvenue</h1>
+<!--Php bienvenue $nom_user-->
+
+<main>
+    <h3>Bienvenue <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname'] . "!!"; ?></h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <!-- A form to logout -->
+    <!-- It redirects to the form controller -->
+    <!-- Note that this could have been done with a simple link and a $_GET parameter -->
+    <form method="post" action="index.php">
+        <fieldset>
+            <legend>Logout</legend>
+            <button type="submit">Logout</button>
+        </fieldset>
+    </form>
     
+</main>
+
+
 </body>
 </html>
