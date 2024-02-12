87a156bc7c84b2087bcf4ce852a886999312624c
Almeida <joao.almeidapinto@etu.estia.fr>
2024-02-12 12:41:19 +0100

    ModelProduitsValides

---
+++ smarttech/scripts/mvc/models/ModelProduitsValides.php
@@ -0,0 +1,27 @@
+<?php
+//--ok--//
+require_once (__DIR__ . '/DBModel.php');
+
+class ProduitsValidesModel extends DBModel{
+
+    function check_login(int $id, string $type, int $etat)
+    {
+    $result = [];
+    $request = "SELECT * FROM produit WHERE id=:id  AND type=:type AND etat=:etat";
+    $statement = $this->db->prepare($request);
+    $statement->execute([
+        "id" => $id,
+        "type" => $type
+        "etat" => $etat
+        ]);
+    $entries = $statement ->fetchAll();
+
+    if (etat == 1){  // Cela renvoi les produits validés
+        $result["id"] = $entries[0]["id"]; 
+        $result["type"] = $entries[0]["type"];
+        $result["etat"] = $entries[0]["etat"];
+    } 
+    return $result;
+    }
+   
+}
