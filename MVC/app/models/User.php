929678e5ad10ebc99b8aa1b7c75ef8c313fb6afd
thatgirlAm2 <amaelle.diop@etu.estia.fr>
2024-02-21 21:07:32 +0100

    nouvelle structure

---
+++ MVC/app/models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+class User{
+    use Model;
+
+    //----Définition de la table concernée----//
+    protected $table = 'employe';
+    //----Colonnes dont la modification est acceptée----//
+    protected $allowedColumns=[
+        'nom',
+        'addresse_mail',
+        'mdp'
+    ];
+
+        function check_login(string $addresse_mail, string $mdp)
+        {
+            $result = [];
+            if(!$this->connect()){
+                return $result;
+            }
+        $data = array(
+            'addresse_mail'=>$addresse_mail,
+            'mdp'=> $mdp); 
+        $entries = $this->first($data);
+        if (!empty($entries)){
+            $result["addresse_mail"] = $entries[0]["addresse_mail"];
+            $result["id"] = $entries[0]["id"];
+        } 
+        else{
+            echo ERROR_LOGIN ; 
+        }
+        
+       return $result; 
+        }
+       
+}
